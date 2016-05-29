5efb9d266bbfac3aa9a74844d56afc5ac74218c1
kunou <t-kunou@esm.co.jp>
2016-05-29 17:02:58 +0900

    First commit

---
+++ app.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+class App < Sinatra::Base
+  get '/servo' do
+    pan = params['pan']
+    thilt = params['thilt']
+
+    if pan || thilt
+      ArduinoFirmata.connect('/dev/cu.usbmodem1411') do
+        servo_write 2, pan.to_i if pan
+        servo_write 3, thilt.to_i if thilt
+      end
+
+      @pan  = pan
+      @thilt = thilt
+    end
+
+    haml :index
+  end
+
+  #get '/streaming' do
+  #  haml :streaming
+  #end
+
+  get '/javascripts/main.js' do
+      coffee :'coffee/main'
+  end
+end
